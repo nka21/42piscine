abc608b245bf02307dab25b65d750a9dada9d572
Naoki Kojima <nkojima@c2r4s4.42tokyo.jp>
2025-02-24 06:58:02 +0900

    c03

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,31 +6,51 @@
 /*   By: nkojima <nkojima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 21:50:53 by nkojima           #+#    #+#             */
-/*   Updated: 2025/02/12 22:13:43 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/02/24 06:55:03 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 unsigned int	ft_strlen(char *str)
 {
-	unsigned int len = 0;
-	while (*str)
-	{
-		str++;
+	unsigned int	len;
+
+	len = 0;
+	while (str[len])
 		len++;
-	}
 	return (len);
 }
 
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int dest_len = ft_strlen(dest);
-	unsigned int src_len = ft_strlen(src);
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
 	if (size <= dest_len)
 		return (size + src_len);
-	while (*dest < size - 1 && *src)
+	i = 0;
+	while (src[i] && (dest_len + i < size - 1))
 	{
-		*dest++ = *src++;
+		dest[dest_len + i] = src[i];
+		i++;
 	}
-	*dest = '\0';
+	dest[dest_len + i] = '\0';
 	return (dest_len + src_len);
 }
+
+// int	main(void)
+// {
+// 	char			dest[] = "ABCDE";
+// 	char			src[] = "1234";
+// 	unsigned int	size;
+// 	unsigned int	result;
+
+// 	size = 2;
+// 	result = ft_strlcat(dest, src, size);
+// 	printf("%s: %u\n", dest, result);
+// 	return (0);
+// }
